0bb84b5212cb7df325440baf6b6a24b00f96eeb9
2024.4.14 <2024.4.14@DESKTOP-OMTIDSQ>
2025-05-28 17:40:41 -0300

    Add project files.

---
+++ WebAppSenac/Models/ContextoEscola.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppSenac.Models
+{
+    public class ContextoEscola : DbContext
+    {
+        public ContextoEscola(DbContextOptions<ContextoEscola> options) : base(options)
+        {
+        }
+        public DbSet<Aluno> Alunos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().ToTable("Alunos");
+        }
+    }
+}
