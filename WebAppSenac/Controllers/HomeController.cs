71229077aa2ae94efd3e3c70815a7e023b9db24b
2024.4.14 <2024.4.14@DESKTOP-OMTIDSQ>
2025-06-02 18:45:27 -0300

    atualização do projeto02/06/2025

--- WebAppSenac/Controllers/HomeController.cs
+++ WebAppSenac/Controllers/HomeController.cs
@@ -27,11 +27,7 @@ namespace WebAppSenac.Controllers
         {
             return View();
         }
-
-        public IActionResult Escola()
-        {
-            return View();
-        }
+    
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
